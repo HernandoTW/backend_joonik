166bcde5f9bd1e0c81adaf9c7f08938e10817777
HernandoTW <97920818+HernandoTW@users.noreply.github.com>
2024-10-24 10:43:37 -0500

    Ajustes

--- test_joonik/routes/api.php
+++ test_joonik/routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LocationController;
 
-// Definimos ruta para obtener sedes
+// Definimos el endpoint para acceder a las sedes
 Route::get('/locations', [LocationController::class, 'index']);
